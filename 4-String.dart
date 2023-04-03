4580728456c609f10260730cac50cd8ff2b351bb
Anes Legendary <93846284+anesmez@users.noreply.github.com>
2023-04-03 06:19:18 +0100

    modifications in String.dart

--- 4-String.dart
+++ 4-String.dart
@@ -30,4 +30,23 @@ void main() {
 
   print(s5);
   print(s6);
+
+  print("-------String Primitive Operations-------");
+  var string = 'String Example';
+  print(string.codeUnits); // print the unicode of each character
+  print(string.length);
+  print(string.isEmpty); // boolean value
+  print(string.isNotEmpty); // boolean value
+  print(string.contains('Example')); // boolean value
+  print(string.startsWith('String')); // boolean value
+  print(string.endsWith('Example')); // boolean value
+  print(string.indexOf('Example')); // return the index of the first occurence of the string
+  print(string.lastIndexOf('Example')); // return the index of the last occurence of the string
+  print(string.substring(0, 6)); // return the substring from index 0 to 6
+  print(string.toLowerCase()); // return the string in lower case
+  print(string.toUpperCase()); // return the string in upper case
+  print(string.trim()); // remove the white spaces from the beginning and the end of the string
+  print(string.split(' ')); // split the string into a list of strings
+  print(string.replaceAll('Example', 'Example2')); // replace all the occurences of the string
+  print(string.replaceFirst('Example', 'Example2')); // replace the first occurence of the string
 }
